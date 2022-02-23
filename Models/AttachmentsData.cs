92fcd9f664d1cf37e68871fd0ef8f7ca77c30fe9
denispankove <denispankove@gmail.com>
2022-02-23 12:33:56 +0200

    added support for workitem attachments

---
+++ Models/AttachmentsData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonToWord.Models
+{
+    public class AttachmentsData
+    {
+        public Uri AttachmentPath { get; set; }
+        public String FileName { get; set; }
+    }
+}
